2321b5f432d9a9c27eee2fc3bb012b0824b41264
DrTim105 <Salihutimothy52@gmail.com>
2020-12-19 00:23:37 +0100

    Android manifest was updated to show splashscreen as launcher page||dependencies were updated

--- app/src/main/java/com/teamx/farmily/SplashScreen.java
+++ app/src/main/java/com/teamx/farmily/SplashScreen.java
@@ -14,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splashscreen);
 
         handler = new Handler();
+        
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
